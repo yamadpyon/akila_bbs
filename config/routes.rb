a9b7ec90f892600c6ae0d1ce54d4fe1ccecae628
krppppp@gmail.com <krppppp@gmail.com>
2019-06-12 11:21:48 +0900

    デザイン2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/2' => 'home#index2'
   get '/3' => 'home#index3'
+  get '/3' => 'home#index3'
+  get '/3' => 'home#index3'
   post 'home/create' =>'home#create'
 end
